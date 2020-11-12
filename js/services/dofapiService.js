3164a0749858051cb18933e13738ab6243214e58
Clément S <clementsimonin00@gmail.com>
2020-11-12 17:50:59 +0100

    Oops mauvais url d'api

--- js/services/dofapiService.js
+++ js/services/dofapiService.js
@@ -2,10 +2,10 @@ app.service('dofapiService', function ($rootScope, $http, $q) {
     const self = this;
 
     // PROD
-    //const baseUrl = "https://magipack.herokuapp.com/dofapi";
+    const baseUrl = "https://magipack.herokuapp.com/dofapi";
 
     // DEV 
-    const baseUrl = "http://localhost:3000/dofapi";
+    // const baseUrl = "http://localhost:3000/dofapi";
 
 
     this.getEquipAsync = (types, startingLevel) => {
